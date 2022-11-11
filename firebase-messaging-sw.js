ae57834c27732c9ddab7368db9a415f1f3cf8e32
Jonatan Nitzan <yognatan33@gmail.com>
2022-11-11 19:18:53 +0200

    completed firebase init

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,10 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCpWLo_qbXG6otMBV1QdyR7qqlx0LSyREU",
-  projectId: "maintenance-manager-113",
-  messagingSenderId: "36279565916",
-  appId: "1:36279565916:web:4ef63a610886b46a01f099",
+  apiKey: 'AIzaSyBfpM45gkV9U0mNCidlQ3AJFPxD38s32HU',
+  appId: '1:36279565916:web:4ef63a610886b46a01f099',
+  messagingSenderId: '36279565916',
+  projectId: 'maintenance-manager-113',
+  authDomain: 'maintenance-manager-113.firebaseapp.com',
+  databaseURL: 'https://maintenance-manager-113-default-rtdb.europe-west1.firebasedatabase.app',
+  storageBucket: 'maintenance-manager-113.appspot.com',
+  measurementId: 'G-YT0W9D6BVG',
 });
 // Necessary to receive background messages:
 const messaging = firebase.messaging();
